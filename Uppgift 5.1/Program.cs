40a28548c2c352d181d841774ad939771400a71f
leonkarlsson1 <leka05001@utb.vaxjo.se>
2023-01-13 11:08:15 +0100

    Add project files.

---
+++ Uppgift 5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Övning_5._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            string[] stad = new string [3];
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Skriv in en stad:");
+                stad[i] = Console.ReadLine();
+                Console.WriteLine($" Du skrev in:" +  stad[i]);
+            }   
+        }
+    }
+}
